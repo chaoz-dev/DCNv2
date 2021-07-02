e87dd7eab770e3e18742d857bdd8f6b6323bbf63
Chao Zhang <chaoz@aurora.tech>
2021-07-02 16:42:36 -0400

    Force cuda flag to force cuda builds.

--- setup.py
+++ setup.py
@@ -29,8 +29,7 @@ def get_extensions():
     extra_compile_args = {"cxx": []}
     define_macros = []
 
-    
-    if torch.cuda.is_available() and CUDA_HOME is not None:
+    if (torch.cuda.is_available() and CUDA_HOME is not None) or (os.getenv("FORCE_CUDA", "0") == "1"):
         extension = CUDAExtension
         sources += source_cuda
         define_macros += [("WITH_CUDA", None)]
@@ -41,9 +40,7 @@ def get_extensions():
             "-D__CUDA_NO_HALF2_OPERATORS__",
         ]
     else:
-        #raise NotImplementedError('Cuda is not available')
-        pass
-    
+        raise NotImplementedError('Cuda is not available')
 
     sources = [os.path.join(extensions_dir, s) for s in sources]
     include_dirs = [extensions_dir]
